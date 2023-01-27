3482a12826b5aeceb267d246b583ff03c2e817e6
katukenda <janithaprashadkatukenda@gmail.com>
2023-01-27 06:53:30 +0530

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Janitha Katukenda on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
